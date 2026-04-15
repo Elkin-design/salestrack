d0344940991382a4535b95afc7a27ad81033f856
aguilarelkin <aguilar.elkin99@gmail.com>
2026-04-14 20:52:27 -0500

    feat: restructure project to support Android app with new MainActivity and AndroidManifest

---
+++ androidApp/build.gradle.kts
@@ -0,0 +1,44 @@
+plugins {
+    alias(libs.plugins.androidApplication)
+    id("org.jetbrains.kotlin.android")
+    alias(libs.plugins.composeCompiler)
+    alias(libs.plugins.googleServices)
+}
+
+android {
+    namespace = "org.salestrack.android"
+    compileSdk = libs.versions.android.compileSdk.get().toInt()
+
+    defaultConfig {
+        applicationId = "org.salestrack.app"
+        minSdk = libs.versions.android.minSdk.get().toInt()
+        targetSdk = libs.versions.android.targetSdk.get().toInt()
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+        }
+    }
+
+    buildFeatures {
+        compose = true
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+}
+
+kotlin {
+    jvmToolchain(11)
+}
+
+dependencies {
+    implementation(projects.composeApp)
+    implementation(libs.androidx.activity.compose)
+    debugImplementation(libs.compose.uiTooling)
+}
--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -1,18 +1,21 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
 
-@Suppress("DEPRECATION")
 plugins {
     alias(libs.plugins.kotlinMultiplatform)
-    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.androidKotlinMultiplatformLibrary)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
     alias(libs.plugins.composeHotReload)
 }
 
-@Suppress("DEPRECATION")
 kotlin {
-    androidTarget {
+    androidLibrary {
+        namespace = "org.salestrack.app"
+        compileSdk = libs.versions.android.compileSdk.get().toInt()
+        minSdk = libs.versions.android.minSdk.get().toInt()
+
         compilerOptions {
             jvmTarget.set(JvmTarget.JVM_11)
         }
@@ -31,14 +34,11 @@ kotlin {
     jvm()
     
     sourceSets {
-        androidMain.dependencies {
-            implementation(libs.compose.uiToolingPreview)
-            implementation(libs.androidx.activity.compose)
-        }
         commonMain.dependencies {
             implementation(libs.compose.runtime)
             implementation(libs.compose.foundation)
             implementation(libs.compose.material3)
+            implementation(libs.compose.material.icons.extended)
             implementation(libs.compose.ui)
             implementation(libs.compose.components.resources)
             implementation(libs.compose.uiToolingPreview)
@@ -46,9 +46,11 @@ kotlin {
             implementation(libs.androidx.lifecycle.runtimeCompose)
             implementation(libs.kotlinx.coroutines.core)
             implementation(libs.kotlinx.datetime)
-            implementation(libs.compose.material.icons.extended)
             implementation(libs.koin.core)
             implementation(libs.koin.compose)
+            implementation(libs.firebaseCommon)
+            implementation(libs.firebaseAuth)
+            implementation(libs.firebaseFirestore)
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -61,37 +63,6 @@ kotlin {
     }
 }
 
-android {
-    namespace = "org.salestrack.app"
-    compileSdk = libs.versions.android.compileSdk.get().toInt()
-
-    defaultConfig {
-        applicationId = "org.salestrack.app"
-        minSdk = libs.versions.android.minSdk.get().toInt()
-        targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
-    }
-    buildTypes {
-        getByName("release") {
-            isMinifyEnabled = false
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-dependencies {
-    debugImplementation(libs.compose.uiTooling)
-}
-
 compose.desktop {
     application {
         mainClass = "org.salestrack.app.MainKt"
@@ -101,5 +72,11 @@ compose.desktop {
             packageName = "org.salestrack.app"
             packageVersion = "1.0.0"
         }
+    }
+}
+
+configurations.named("jvmMainCompileClasspath") {
+    attributes {
+        attribute(KotlinPlatformType.attribute, KotlinPlatformType.jvm)
     }
 }
